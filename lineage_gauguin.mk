80d79714d93c14043aa1157bc4941b04a1dbba41
inturvert <irkarharsharaj@gmail.com>
2026-03-03 20:35:25 +0530

    package_removal: To remove prebuilt Apps

--- lineage_gauguin.mk
+++ lineage_gauguin.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/gauguin/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit Package Removal
+$(call inherit-product, device/xiaomi/gauguin/package_removal/Android.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_gauguin
 PRODUCT_DEVICE := gauguin
@@ -25,3 +28,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="gauguin-user 12 RKQ1.200826.002 V14.0.2.0.SJSMIXM release-keys" \
     BuildFingerprint=Xiaomi/gauguin_global/gauguin:12/RKQ1.200826.002/V14.0.2.0.SJSMIXM:user/release-keys
+    
+
---
+++ package_removal/Android.mk
@@ -0,0 +1,12 @@
+PRODUCT_PACKAGES -=
+	Browser2 \
+	BCR \
+	Recorder \
+	Seedvault \
+	FMRadio \
+	Gallery2 \
+	Glimpse \
+	Jelly \
+	Music \
+	MusicFX \
+	Twelve
