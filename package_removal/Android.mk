d38a8fdb01181f1e6fc5bffc4046f87d23e2c0b1
inturvert <irkarharsharaj@gmail.com>
2026-03-14 16:54:48 +0530

    RemovePkgs module to remove unwanted apps

--- package_removal/Android.mk
+++ package_removal/Android.mk
@@ -1,6 +1,13 @@
-PRODUCT_PACKAGES -=
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_OVERRIDES_PACKAGES := \
 	Browser2 \
-	BCR \
+	Bcr \
 	Recorder \
 	Seedvault \
 	FMRadio \
@@ -10,3 +17,10 @@ PRODUCT_PACKAGES -=
 	Music \
 	MusicFX \
 	Twelve
+   
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
+
+
