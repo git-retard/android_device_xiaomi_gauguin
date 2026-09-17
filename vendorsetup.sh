9ed32b960ee8e25d5b05272eb791c0abc5c3a3c9
git-retard <irkarharsharaj@gmail.com>
2026-09-17 21:02:09 +0530

    vendorsetup.sh: device required trees

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone -b lineage-23.2 https://github.com/git-retard/proprietary_vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
+git clone -b lineage-23.2 https://github.com/git-retard/android_kernel_xiaomi_gauguin.git kernel/xiaomi/gauguin
+git clone -b lineage-23.2 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
